e1a793049bdefdc876e46f87c6391a33c854ebfd
samixchoumi <samixchoumi@gmail.com>
2020-01-08 16:46:21 +0100

    Bug des NaN moyenne

--- main_erreur.py
+++ main_erreur.py
@@ -23,8 +23,50 @@ def main():
 	implémenter ces lois dans votre application précédente				
     '''
     # On charge le fichier csv
+    # dtype={'Mars': np.int64, 'Juin': np.int64, 'Juillet': np.int64, 'Octobre': np.int64}
     data_erreur = pd.read_csv('data/data_SI_erreur.csv', sep=';', encoding="utf-8")
+    print(data_erreur)
+    # data_erreur = data_erreur.apply(pd.to_numeric, errors='coerce')
+    print('------------------------------')
+    for column in data_erreur:
+        val_column = data_erreur[column]
+        print(val_column)
+        for value in val_column:
+            print(value)
+            if not value.isnumeric:
+                value = val_column.mean()
+        print(val_column)
+        '''
+        if not val_column.value.isnumeric:
+            print(val_column)
+            val_column = val_column.mean()
+        '''
+    print(data_erreur)
+    # data_erreur = data_erreur.apply(lambda x: x.fillna(x.mean()))
+    # print(data_erreur)
+    
+    '''
+    for data in data_erreur:
+        for value in data_erreur[data]:
+            if not isinstance(value, int) or not isinstance(value, float):
+                print(value)
+                print(type(value))
+                value = value.replace(value, 999)
 
+    if isinstance(data_erreur, np.float64):
+        data_erreur = data_erreur.astype('int64').dtypes
+    if not isinstance(data_erreur, np.int64):
+        data_erreur = data_erreur.replace(data_erreur, 999)
+    '''
+    '''
+    for dataset in data_erreur_numeric:
+        mars_avg = dataset['mars'].mean()
+        mars_std = dataset['mars'].std()
+        mars_null_count = dataset['mars'].isnull().sum()
+        mars_null_random_list = np.random.randint(mars_avg - mars_std, mars_avg + mars_std, size=mars_null_count)    
+        dataset.loc[np.isnan(dataset['mars']),'mars'] = mars_null_random_list
+    '''
+    
     # Partie sur les statistiques
     with open("results/Resultats_erreur.txt", "w") as text_file:
         print("Moyennes : ", file=text_file)
